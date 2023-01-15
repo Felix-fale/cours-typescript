0e15fb9b56c28d8f71172dbb855ea21f18277411
Felix-fale <falefelix23@gmail.com>
2023-01-15 23:47:24 +0000

    feat: 4.Data structures and types

--- types.ts
+++ types.ts
@@ -1,35 +1,8 @@
-let myVariable;
-myVariable = 'alex';
-myVariable = 27;
-myVariable = true;
+let friends: (number | string)[] = [];
+friends= [27, 25, 'string'];
 
-let age  = 27;
-age = 28;
-// age = 'string';
-
-let myName = 'alex';
-myName = 'justine';
-// myName = true;
-
-let a: any;
-let x: number;
-let y: string;
-let z: boolean;
-
-a = 27;
-a = true;
-a = 'string';
-
-x = 27;
-x = true;
-x = 'string';
-
-y = 27;
-y = true;
-y = 'string';
-
-z = 27;
-z = true;
-z = 'string';
-
-let myFavouriteNumber: number = 17;
+let userData: {name: string; age: number; isMajeur: true} = {
+    name: "alexis",
+    age: 27,
+    isMajeur: true
+};
