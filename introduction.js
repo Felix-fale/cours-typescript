8a16388ce80939a96a7a851a788ad00b48b6c3aa
Felix-fale <falefelix23@gmail.com>
2023-01-15 15:03:39 +0000

    first commit

---
+++ introduction.js
@@ -0,0 +1,24 @@
+// When a particular type feels like it's useful in most
+// codebases, they are added into TypeScript and become
+// available for anyone which means you can consistently
+// rely on their availability
+var navigationInfo = {
+    home: { title: "Home", url: "/" },
+    about: { title: "About", url: "/about" },
+    contact: { title: "Contact", url: "/contact" },
+    stickers: { title: "Stickers", url: "/stickers/all" }
+};
+// InstanceType<Type>
+// Creates a type which is an instance of a class, or object
+// with a constructor function.
+var StickerCollection = /** @class */ (function () {
+    function StickerCollection() {
+    }
+    return StickerCollection;
+}());
+// ThisType<Type>
+// Unlike other types, ThisType does not return a new
+// type but instead manipulates the definition of this
+// inside a function. You can only use ThisType when you
+// have noImplicitThis turned on in your TSConfig.
+// https://www.typescriptlang.org/docs/handbook/utility-types.html
