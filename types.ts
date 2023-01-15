4df2c72c75e9c2d4ad5543955b5a189c3a16c3e1
Felix-fale <falefelix23@gmail.com>
2023-01-15 23:31:32 +0000

    feat: 3.How to declare our types

---
+++ types.ts
@@ -0,0 +1,35 @@
+let myVariable;
+myVariable = 'alex';
+myVariable = 27;
+myVariable = true;
+
+let age  = 27;
+age = 28;
+// age = 'string';
+
+let myName = 'alex';
+myName = 'justine';
+// myName = true;
+
+let a: any;
+let x: number;
+let y: string;
+let z: boolean;
+
+a = 27;
+a = true;
+a = 'string';
+
+x = 27;
+x = true;
+x = 'string';
+
+y = 27;
+y = true;
+y = 'string';
+
+z = 27;
+z = true;
+z = 'string';
+
+let myFavouriteNumber: number = 17;
