075a6e943cbeac22978010f002f234efab24edb8
Felix-fale <falefelix23@gmail.com>
2023-01-16 23:39:23 +0000

    inheritance with interfaces

--- types.js
+++ types.js
@@ -1,8 +1,13 @@
 "use strict";
+var user2 = {};
+user2.firstname = "justine";
+console.log(user2.firstname);
 var user1 = {
     firstname: "alexis",
     nickname: "ayu",
-    // age: 27,
-    password: "idk",
+    age: 27,
+    hobbies: ["lire", "basket"],
+    color: "crimson",
+    isMajeur: true
 };
 console.log(user1);
--- types.ts
+++ types.ts
@@ -1,15 +1,28 @@
 interface IUserProfile {
-    readonly firstname: string;
+    firstname: string;
     nickname: string;
     age?: number;
-    password: string;
 }
 
-const user1: IUserProfile={
+interface IAdvanceUserProfile extends IUserProfile{
+    hobbies: string[];
+    color: string;
+    isMajeur: boolean;
+}
+
+const user2 = <IAdvanceUserProfile>{};
+
+user2.firstname = "justine";
+console.log(user2.firstname);
+
+
+const user1: IAdvanceUserProfile={
     firstname: "alexis",
     nickname: "ayu",
-    // age: 27,
-    password: "idk",
+    age: 27,
+    hobbies: ["lire", "basket"],
+    color: "crimson",
+    isMajeur: true
 }
 
 console.log(user1);
